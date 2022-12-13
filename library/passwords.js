4de6859a1f514f993e01bbf4db26eb8ed1c5cb37
Abhishek Mishra <mishraabhi0123@gmail.com>
2022-12-13 16:44:42 +0530

    Added js doc

--- library/passwords.js
+++ library/passwords.js
@@ -1,5 +1,9 @@
 const bcrypt = require("bcrypt");
-
+/**
+ * The encryptPassword function is an asynchronous function that uses the bcrypt library to hash a given password. The function accepts a password argument, which is the password that needs to be hashed.
+ * @param {*} password 
+ * @returns 
+ */
 async function encryptPassword(password) {
   if (!password) {
     throw new NamedError("Invalid password")
@@ -8,7 +12,12 @@ async function encryptPassword(password) {
   return hash;
 }
 
-
+/**
+ * The comparePassword function is an asynchronous function that uses the bcrypt library to compare a given password to a hashed password. The function accepts two arguments: password, which is the plain-text password to be compared, and hash, which is the hashed password to compare against.
+ * @param {*} password 
+ * @param {*} hash 
+ * @returns 
+ */
 async function comparePassword(password, hash) {
   console.log({ password, hash })
   if (!(password && hash)) {
